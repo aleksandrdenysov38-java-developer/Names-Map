854450ebe8a6e3d5438aaf41fcfbe365ca521308
alex <aleksandrdenysov38@gmail.com>
2026-03-18 23:17:52 +0200

    Ready project

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is my app");
+        DataHandler handler = new DataHandler();
+        UIOperator uiOperator = new UIOperator();
+        uiOperator.getOutput(handler.getAll());
+        uiOperator.getOutput(handler.getById(172));
     }
 }
+
+
